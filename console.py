a26480b6ce420d0da6f3e6780169ca9bec0dc271
PuzzleEmptyM <emptympuzzle@gmail.com>
2023-10-05 14:17:27 -0700

    Updated console docstrings

--- console.py
+++ console.py
@@ -6,35 +6,31 @@ import cmd
 
 class HBNBCommand(cmd.Cmd):
     """ Set the command prompt """
-    prompt = '(hbnb)'
+    prompt = "(hbnb)"
 
     def do_create(self, args):
         """Create a new object"""
-        # Implement logic to create a new object
         print("Object created")
 
     def do_retrieve(self, args):
         """Retrieve an object"""
-        # Implement logic to retrieve an object
         print("Object retrieved")
 
     def do_operations(self, args):
         """Perform operations on objects"""
-        # Implement operations like counting, computing stats, etc.
         print("Operations performed")
 
     def do_update(self, args):
         """Update attributes of an object"""
-        # Implement logic to update object attributes
         print("Object attributes updated")
 
     def do_destroy(self, args):
         """Destroy an object"""
-        # Implement logic to destroy an object
         print("Object destroyed")
 
     def do_quit(self, args):
         """Exit the command interpreter"""
+        print("Logging out...")
         return True
 
 
