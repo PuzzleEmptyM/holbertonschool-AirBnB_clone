4b0840f029846783693fe553708fb680a1c0cf3e
colbertamir <colbertamir@gmail.com>
2023-10-05 13:27:13 -0700

    console guideline

---
+++ console.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""Defines the HBnB console."""
+
+
+import cmd
+
+class AirBnbCloneInterperter(cmd.Cmd):
+    prompt = "> "  # Set the command prompt
+
+    def do_create(self, args):
+        """Create a new object"""
+        # Implement logic to create a new object
+        print("Object created")
+
+    def do_retrieve(self, args):
+        """Retrieve an object"""
+        # Implement logic to retrieve an object
+        print("Object retrieved")
+
+    def do_operations(self, args):
+        """Perform operations on objects"""
+        # Implement operations like counting, computing stats, etc.
+        print("Operations performed")
+
+    def do_update(self, args):
+        """Update attributes of an object"""
+        # Implement logic to update object attributes
+        print("Object attributes updated")
+
+    def do_destroy(self, args):
+        """Destroy an object"""
+        # Implement logic to destroy an object
+        print("Object destroyed")
+
+    def do_quit(self, args):
+        """Exit the command interpreter"""
+        return True
