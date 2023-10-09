c2434f26e198daa2184deed07e4157ba2f0e0495
PuzzleEmptyM <emptympuzzle@gmail.com>
2023-10-09 12:53:57 -0700

    Updated docstrings for consistency

--- models/state.py
+++ models/state.py
@@ -4,7 +4,8 @@ from models.base_model import BaseModel
 
 
 class State(BaseModel):
-    """Represent a state.
+    """
+    Represent a state.
 
     Attributes:
         name (str): The name of the state.
